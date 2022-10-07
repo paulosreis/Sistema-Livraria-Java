0e914c864f49b0cb8481f7c65704295a035bfb00
Paulo G Reis <paulosantos--@live.com>
2022-10-06 20:00:03 -0400

    inicia repositório e configura projeto com spring boot

---
+++ src/test/java/com/grupo6/Sistemalivraria/SistemaLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.Sistemalivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
